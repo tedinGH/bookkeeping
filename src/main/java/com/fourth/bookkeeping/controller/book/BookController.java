e0294030fde482ea8d21bc2140a921a6203224da
Mason Don <542515190@163.com>
2018-06-08 00:44:58 +0800

    新增根据userid查询接口和更新接口

--- src/main/java/com/fourth/bookkeeping/controller/book/BookController.java
+++ src/main/java/com/fourth/bookkeeping/controller/book/BookController.java
@@ -2,12 +2,17 @@ package com.fourth.bookkeeping.controller.book;
 
 import com.fourth.bookkeeping.domain.Book;
 import com.fourth.bookkeeping.reposity.BookRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("book")
@@ -20,6 +25,21 @@ public class BookController {
     public boolean add(Book book){
         book.setOptime(new Date());
         book.setUserId(1);
+        bookRepository.save(book);
+        return true;
+    }
+
+    @RequestMapping(value = "{userId}", method = RequestMethod.GET)
+    public List<Book> get(@PathVariable int userId){
+        List<Book> books = bookRepository.findByUserIdOrderByOptimeDesc(userId);
+//        Page<Book> books = bookRepository.findAll(PageRequest.of(0,10,new Sort(Sort.Direction.DESC, "optime")));
+//        return books.getContent();
+        return books;
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.PUT)
+    public boolean update(Book book){
+        book.setOptime(new Date());
         bookRepository.save(book);
         return true;
     }
--- src/main/java/com/fourth/bookkeeping/reposity/BookRepository.java
+++ src/main/java/com/fourth/bookkeeping/reposity/BookRepository.java
@@ -3,5 +3,10 @@ package com.fourth.bookkeeping.reposity;
 import com.fourth.bookkeeping.domain.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookRepository extends JpaRepository<Book,Integer>{
+
+    List<Book> findByUserIdOrderByOptimeDesc(int userId);
+
 }
